d2db0feea23bf076c6f836ac38684ab6d554eefa
Mauritz Erick <mauritzerick@Mauritzs-MacBook-Air.local>
2025-05-02 14:45:16 +1000

    working on footer..

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import ExperienceSection from "./sections/ExperienceSection";
 import TechStack from "./sections/TechStack";
 import Testimonials from "./sections/Testimonials";
 import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
 const App = () => (
   <>
@@ -19,6 +20,7 @@ const App = () => (
     <TechStack />
     <Testimonials />
     <Contact />
+    <Footer />
   </>
 );
 
