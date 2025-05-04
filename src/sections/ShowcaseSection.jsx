915aeab59c5b15fb731f0b8f57e719ef0a3173b6
Mauritz Erick <mauritzerick@Mauritzs-MacBook-Air.local>
2025-05-04 16:56:34 +1000

    website repositioning

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -49,17 +49,59 @@ const AppShowcase = () => {
         <div className="showcaselayout">
           <div ref={rydeRef} className="first-project-wrapper">
             <div className="image-wrapper">
-              <img src="/images/project1.png" alt="Ryde App Interface" />
+            <a
+                href="https://www.winnings.com.au"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+              <img src="/images/project6.png" alt="Ryde App Interface" />
+              </a>
             </div>
-            <div className="text-content">
-              <h2>
+            <div className="text-content mb-20">
+            <h2>
+                <a
+                    href="https://www.winnings.com.au"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >Winnings – World Leading Appliance Brands
+                </a>
+                </h2>
+              {/* <h2>
                 (WIP) - On-Demand Rides Made Simple with a Powerful, User-Friendly App
                 called Ryde
-              </h2>
-              <p className="text-white-50 md:text-xl">
+              </h2> */}
+              {/* <p className="text-white-50 md:text-xl">
                 An app built with React Native, Expo, & TailwindCSS for a fast,
                 user-friendly experience.
-              </p>
+              </p> */}
+            </div>
+
+            <div className="image-wrapper">
+            <a
+                href="https://www.winnings.com.au"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+              <img src="/images/project5.png" alt="Ryde App Interface" />
+              </a>
+            </div>
+            <div className="text-content">
+            <h2 mb-20>
+                <a
+                    href="https://www.appliancesonline.com.au"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >Appliances Online Australia
+                </a>
+                </h2>
+              {/* <h2>
+                (WIP) - On-Demand Rides Made Simple with a Powerful, User-Friendly App
+                called Ryde
+              </h2> */}
+              {/* <p className="text-white-50 md:text-xl">
+                An app built with React Native, Expo, & TailwindCSS for a fast,
+                user-friendly experience.
+              </p> */}
             </div>
           </div>
 
@@ -67,23 +109,23 @@ const AppShowcase = () => {
             <div className="project" ref={libraryRef}>
               <div className="image-wrapper bg-[#ffffff]">
               <a
-                href="https://www.appliancesonline.com.au"
+                href="https://www.homeclearance.com.au"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <img
-                  src="/images/project5.png"
-                  alt="AOL"
+                  src="/images/project7.png"
+                  alt="HC"
                 />
               </a>
               </div>
               <h2>
                 <a
-                  href="https://www.appliancesonline.com.au"
+                  href="https://www.homeclearance.com.au"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Appliances Online Australia
+                  Home Clearance - High Quality Brands, Low Cost
                 </a>
               </h2>
             </div>
@@ -107,23 +149,24 @@ const AppShowcase = () => {
                 </h2>
             </div>
             <div className="project" ref={ycDirectoryRef}>
-              <div className="image-wrapper bg-[#000000]">
+              <div className="image-wrapper bg-[#040404]">
               <a
-                href="https://www.winnings.com.au"
+                href="https://docs.helloclever.co/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img src="/images/project6.png" alt="Winnings" />
+                <img src="/images/project8.png" alt="helloclever" />
                 </a>
               </div>
               <h2>
                 <a
-                    href="https://www.winnings.com.au"
+                    href="https://docs.helloclever.co/"
                     target="_blank"
                     rel="noopener noreferrer"
-                  >Winnings – World Leading Appliance Brands
+                  >Hello Clever Developer Documentation
                 </a>
                 </h2>
+              
             </div>
           </div>
         </div>
